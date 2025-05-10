c5f1f56387b4b5b8df50f8fd90be171b1719897e
galaxy169 <adilansari689@gmail.com>
2025-05-10 14:55:52 +0530

    Added redux store, added product detail pages with Dynamic routing

--- src/app/redux/store/slices/cartSlice.js
+++ src/app/redux/store/slices/cartSlice.js
@@ -18,19 +18,9 @@ const cartSlice = createSlice({
       }
     },
 
-    removeFromCart(state, action) {
-      state.cartItems = state.cartItems.filter(
-        (item) => item.id !== action.payload0
-      );
-    },
-
-    updateQuantity(state, action) {
-      const { id, quantity } = action.payload;
-      const item = state.cartItems.find((product) => p.id === id);
-      if (item) item.quantity = quantity;
-    },
+    
   },
 });
 
-export const { addToCart, removeFromCart, updateQuantity } = cartSlice.actions;
+export const { addToCart } = cartSlice.actions;
 export default cartSlice.reducer;
